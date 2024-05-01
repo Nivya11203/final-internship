e4e58032c8e292fbb8da1e98d367edb04a545d2d
Nivya11203 <154998561+Nivya11203@users.noreply.github.com>
2024-05-01 17:47:50 +0530

    Add files via upload

---
+++ autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I'm Nivedita Sakhare</span> a passionate web developer with a love for crafting digital experiences that leave a lasting impact. My journey into the realm of coding began with a curiosity-driven dive into HTML and CSS, and since then, I've been on a continuous quest to expand my skills and stay ahead of the curve. From responsive designs to seamless user experiences, I thrive on the challenge of turning ideas into innovative web solutions.<br><br>Beyond the tech and the hobbies, I believe in fostering meaningful connections and collaborating with like-minded individuals to bring visions to life. So let's connect, create, and maybe even share a game of badminton or a dance session along the way!.",
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
